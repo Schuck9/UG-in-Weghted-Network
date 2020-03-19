2f7c6778ccea99d0aa7d945743e9aa2c8028fef7
Schuck9 <34061849+Schuck9@users.noreply.github.com>
2020-03-19 21:45:25 +0800

    update

--- viz.py
+++ viz.py
@@ -1,6 +1,6 @@
 """
 Ultimatum Game in complex network Visualization 
-@date: 2020.3.2
+@date: 2020.3.19
 @author: Tingyu Mo
 """
 
@@ -70,26 +70,15 @@ def avg_pq_viz():
     '''
     Figure 2 like
     '''
-    u = 0.001
-    save_path = "./result/u_{}.jpg".format(u)
+    u = 0.1
+    info = 'RG_Weighted_0.4'
+    save_path = "./result/{}_u_{}.jpg".format(info,u)
     x_label = [0.001,0.01,0.1,1,10]
     x_axis = np.log10(x_label)
-    #u = 0.001
     avg_list = [ (0.5,0.5),
-                            (0.4403132002723732, 0.49710342183561984),(0.38107634048643024, 0.22466776945791456),
-                            (0.23585496566849598, 0.1457249920728233),(0.1213658889484079, 0.008875168335401846),
+                            (0.494011917,0.496625418),(0.498278643,0.471188505),
+                            (0.341997159,0.261274376),(0.124914813,0.115971024),
                             ]
-        
-    #u=0.01
-    # avg_list = [ (0.5,0.5),
-    #                     (0.49562128010778167, 0.45172168140157937),(0.4640309056507617, 0.36385078594499665),
-    #                     (0.18879100191623888, 0.18328685482754506),(0.1114854687478263, 0.10302883646686213),
-    #                                                                                                                                                                                                                         ]              
-    #u=0.1                                                                                                                                                                                                                                                                                                                                                                                                                           ]
-    # avg_list = [ (0.5,0.5),
-    #                     (0.5020319102721155, 0.5006569915894589),(0.5022221567143004, 0.4692042485477485),
-    #                     (0.33209948374867243, 0.254205750623632),(0.13301475283222816, 0.13198493551101176),
-    #                                                                                                                                                                                                                         ]
     p_axis = list()
     q_axis = list()
     for stg in avg_list:
@@ -98,7 +87,10 @@ def avg_pq_viz():
         q_axis.append(q)
 
     plt.figure()
-    plt.title("u={}".format(u))
+    # plt.rcParams['font.family'] = ['sans-serif']
+    # plt.rcParams['font.sans-serif'] = ['SimHei']
+    # plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+    plt.title(" {} u={}".format(info,u))
     plt.xlabel("Selection strength(w)")#x轴p上的名字
     plt.ylabel("Mean")#y轴上的名字
     plt.xticks(x_axis,x_label,fontsize=16)
@@ -111,12 +103,82 @@ def avg_pq_viz():
     plt.show()
 
 
+def data_loader(data_path):
+    
+    weight_axis = [0.25, 0.3, 0.35, 0.4, 0.55, 0.7, 0.85]
+    weight_axis = [str(x )for x in weight_axis]
+    u = [0.001, 0.01, 0.1]
+    u = [str(i) for i in u]
+    w = [0.001, 0.01, 0.1, 1, 10]
+    w = [str(i) for i in w]
+    data = pd.read_excel(data_path)
+    data_dict = dict()
+    for weight_key in weight_axis:
+        data_dict[weight_key] = dict()
+        for u_key in u:
+            data_dict[weight_key][u_key] =dict()
+            # for w_key in w:
+            #     data_dict[weight_key][u_key][w_key] = np.zeros([1,2])
+    
+    pq_data =data[['p','q']].dropna()
+    for i,weight_key in enumerate(weight_axis):
+        weight_data = pq_data.iloc[15*i:15*(i+1)]
+        
+        for j,u_key in enumerate(u):
+            u_data = weight_data.iloc[5*j:5*(j+1)].values
+            for k,w_key in enumerate(w):
+                # print(u_data[k])
+                data_dict[weight_key][u_key][w_key] = u_data[k]
+    print("data loaded!")
+    return data_dict
 
+def weighted_graph_viz():
+    data_path ='./result/data.xlsx'
+    data_dict= data_loader(data_path)
+    weight_axis = [0.25, 0.3, 0.35, 0.4, 0.55, 0.7, 0.85]
+    weight_axis_str = [str(x )for x in weight_axis]
+    x_axis = weight_axis
+    pq = ['Offer(p)','Demond(q)']
+    u = [0.001, 0.01, 0.1]
+    u = [str(i) for i in u]
+    w = [0.001, 0.01, 0.1, 1, 10]
+    w = [str(i) for i in w]
+    
+    for k ,role in enumerate(pq):
+        for u_ in u:
+            y_list = []
+            for w_ in w:
+                ls = []
+                for i,weight_key in enumerate(weight_axis_str):
+                    ls.append(data_dict[weight_key][u_][w_][k])
+                y_list.append(ls)
 
+            # print("y_axis done!")
+
+            info_str = role+"_"+u_
+            save_path = './result/{}.jpg'.format(info_str)
+            plt.figure()
+            
+            # plt.rcParams['font.sans-serif']=['SimHei']
+            # plt.rcParams['axes.unicode_minus'] = False
+            plt.title(info_str)
+            plt.xlabel("weight")#x轴p上的名字
+            plt.ylabel("{}".format(role))#y轴上的名字
+            plt.plot(x_axis, y_list[0] ,marker='>',linestyle='-',color='purple', label='w = 0.001')
+            plt.plot(x_axis, y_list[1] ,marker='^',linestyle='-',color='skyblue', label='w = 0.01')
+            plt.plot(x_axis, y_list[2], marker='s',linestyle='-',color='green', label='w = 0.1')
+            plt.plot(x_axis, y_list[3], marker='*',linestyle='-',color='red', label='w = 1')
+            plt.plot(x_axis, y_list[4], marker='x',linestyle='-',color='black', label='w = 10')
+            # plt.plot(x_axis, thresholds, color='blue', label='threshold')
+            plt.legend(loc = 'upper right') # 显示图例
+            plt.savefig(save_path)
+            print("Figure has been saved to: ",save_path)
+            plt.show()
 
 if __name__ == '__main__':
 
     # RecordName ='2020-03-03-09-14-20'   
     # time_option = "all"
     # pq_distribution_viz(RecordName,time_option)
-    avg_pq_viz()
+    # avg_pq_viz()
+    weighted_graph_viz()
